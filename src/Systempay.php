749c8fe65dd4224e0fe81fa7e3face745da4e462
Sebastien HEYD <sebastienheyd@users.noreply.github.com>
2019-04-02 11:35:44 +0200

    Allow "0" as a value

--- src/Systempay.php
+++ src/Systempay.php
@@ -68,7 +68,7 @@ class Systempay
         }
 
         foreach ($param as $k => $v) {
-            if (empty($v)) {
+            if ($v !== '0' && empty($v)) {
                 continue;
             }
 
